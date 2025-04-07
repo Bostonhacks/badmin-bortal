06f09de7bf697204954f15661b43956c212fe611
Tiffany Liu <mazingmonkey2004@gmail.com>
2025-04-07 18:53:14 -0400

    partially done

---
+++ bostonhacks-admin-dashboard/src/data/dummyData.js
@@ -0,0 +1,31 @@
+// dummy data...
+
+export const dummyApps = [
+    {
+        id: "12345",
+        gender: "Man",
+        pronouns: "He / Him / His",
+        age: "20",
+        ethnicity: "Prefer Not to Answer",
+        gradYear: "2026",
+        phoneNumber: "18572004402",
+        school: "Boston University",
+        city: "Boston",
+        state: "Massachusetts",
+        conutry: "USA",
+        educationLevel: "Undergraduate University",
+        major: "Information Systems / Information Technology / System Administration",
+        diet: "",
+        shirtSize: "M",
+        sleep: true,
+        github: "https://github.com/wega-syf",
+        linkedin: "https://www.linkedin.com/in/wega-syafitra-winarso/?trk=public_profile_browsemap&originalSubdomain=id",
+        portfolio: "",
+        whyBostonhacks: "An opportunity to push my skill in building applications in a very short period of time, connect with other amazing people, learn business skills",
+        applicationYear: "2025",
+        user: "kI6rcgzUTLP2KW4c4BY1XSfnMiU2",
+        userId: "kI6rcgzUTLP2KW4c4BY1XSfnMiU2",
+        status: PENDING
+    }
+]
+
