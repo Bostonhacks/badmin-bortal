11d0b6e48c43381cd54bfd154c0359eca98ce1a9
alicehan8 <alicehan@bu.edu>
2025-07-24 22:02:01 -0400

    creating table

--- bostonhacks-admin-dashboard/src/app/applications/page.jsx
+++ bostonhacks-admin-dashboard/src/app/applications/page.jsx
@@ -64,105 +64,108 @@ export default function ApplicationPage() {
             pending: data.filter(app => app.status === 'Pending').length
         };
         setStatusCounts(counts);
-    }, []);
+    });
+}, []);
 
-    const handleOpenModal = useCallback((applicant) => {
-        setSelectedApplicant(applicant);
-        setModalOpen(true);
-    }, []);
+const handleOpenModal = (applicant) => {
+    setSelectedApplicant(applicant);
+    setModalOpen(true);
+};
 
-    const handleCloseModal = () => {
-        setModalOpen(false);
-    };
+const handleCloseModal = () => {
+    setModalOpen(false);
+};
 
-    // Define the columns for DataGrid
-    const columns = [
-        { field: 'id', headerName: 'ID', flex: 0.5 },
-        {
-            field: 'firstName',
-            headerName: 'First name',
-            flex: 1, // use flex 1,2,3,4... for the width of the column
-            //valueGetter: (params) => `${params.row.firstName || ''} ${params.row.lastName || ''}`,
-        },
-        {
-            field: 'lastName',
-            headerName: 'Last Name',
-            flex: 1, // use flex 1,2,3,4... for the width of the column
-        },
-        { field: 'email', headerName: 'Email', flex: 1 },
-        { field: 'schoolLabel', headerName: 'School', flex: 1 },
-        { field: 'github', headerName: 'GitHub', flex: 1 },
-        {
-            field: 'actions',
-            headerName: 'Actions',
-            sortable: false,
-            flex: 2,
-            renderCell: (params) => (
-                <>
-                    <Button
-                        variant="contained"
-                        color="primary"
-                        style={{ backgroundColor: '#1976D2', color: 'white', marginRight: 8 }}
-                        onClick={() => handleOpenModal(params.row)}
-                    >
-                        View
-                    </Button>
-                    <Button
-                        variant="contained"
-                        style={{ backgroundColor: '#4CAF50', color: 'white', marginRight: 8 }}
-                    >
-                        Accept
-                    </Button>
-                    <Button
-                        variant="contained"
-                        style={{ backgroundColor: '#F44336', color: 'white' }}
-                    >
-                        Reject
-                    </Button>
-                </>
-            ),
-        },
-    ];
+// Define the columns for DataGrid
+const columns = [
+    {
+        field: 'firstName',
+        headerName: 'First name',
+        flex: 1, // use flex 1,2,3,4... for the width of the column
+        //valueGetter: (params) => `${params.row.firstName || ''} ${params.row.lastName || ''}`,
+    },
+    {
+        field: 'lastName',
+        headerName: 'Last Name',
+        flex: 1, // use flex 1,2,3,4... for the width of the column
+    },
+    { field: 'email', headerName: 'Email', flex: 1 },
+    { field: 'schoolLabel', headerName: 'School', flex: 1 },
+    { field: 'github', headerName: 'GitHub', flex: 1 },
+    {
+        field: 'actions',
+        headerName: 'Actions',
+        sortable: false,
+        flex: 2,
+        renderCell: (params) => (
+            <>
+                <Button
+                    variant="contained"
+                    color="primary"
+                    style={{ backgroundColor: '#1976D2', color: 'white', marginRight: 8 }}
+                    onClick={() => handleOpenModal(params.row)}
+                >
+                    View
+                </Button>
+                <Button
+                    variant="contained"
+                    style={{ backgroundColor: '#4CAF50', color: 'white', marginRight: 8 }}
+                >
+                    Accept
+                </Button>
+                <Button
+                    variant="contained"
+                    style={{ backgroundColor: '#F44336', color: 'white' }}
+                >
+                    Reject
+                </Button>
+            </>
+        ),
+    },
+];
 
-    // Convert applicants to rows for the DataGrid
-    const rows = applicants.map((applicant) => ({
-        id: applicant.id,
-        firstName: applicant.firstName,
-        lastName: applicant.lastName,
-        email: applicant.email,
-        schoolLabel: applicant.schoolLabel,
-        github: applicant.github,
-    }));
+// Convert applicants to rows for the DataGrid
+const rows = applicants.map((applicant) => ({
+    id: applicant.id,
+    firstName: applicant.firstName,
+    lastName: applicant.lastName,
+    email: applicant.email,
+    schoolLabel: applicant.schoolLabel,
+    github: applicant.github,
+}));
 
-    // Try adding a double/single click event listener to the DataGrid (part of API) that takes you to the specific page of the applicant (use the id field and call /api/applicant/[id] to get the specific applicant data)
-    // by possibly rerouting to /applicants/[id] or /applicants/[id]/view
-    const pageStyles = {
-        backgroundColor: '#1E1E1E', // Dark background for the entire page
-        color: 'white', // White text color
-        height: '100vh', // Full viewport height
-        padding: '2rem', // Padding around the content
-    };
+// Try adding a double/single click event listener to the DataGrid (part of API) that takes you to the specific page of the applicant (use the id field and call /api/applicant/[id] to get the specific applicant data)
+// by possibly rerouting to /applicants/[id] or /applicants/[id]/view
+const pageStyles = {
+    // backgroundColor: '#1E1E1E', // Dark background for the entire page
+    // color: 'white', // White text color
+    // height: '100vh', // Full viewport height
+    padding: '2rem', // Padding around the content
+};
 
-    const cardStyles = {
-        backgroundColor: '#252526', // Slightly lighter shade for cards
-        borderRadius: '8px', // Rounded corners for the cards
-        margin: '1rem', // Margin around cards
-        padding: '1rem', // Padding inside cards
-        boxShadow: '0 4px 8px 0 rgba(0,0,0,0.2)', // Subtle shadow for depth
-        color: 'white', // White text color
-    };
+const cardStyles = {
+    backgroundColor: '#252526', // Slightly lighter shade for cards
+    borderRadius: '8px', // Rounded corners for the cards
+    margin: '1rem', // Margin around cards
+    padding: '1rem', // Padding inside cards
+    boxShadow: '0 4px 8px 0 rgba(0,0,0,0.2)', // Subtle shadow for depth
+    color: 'white', // White text color
+};
 
-    return (
-        <main style={pageStyles}>
-            <div style={{ ...cardStyles, display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+return (
+    <main style={pageStyles}>
+        <h2 className="text-4xl font-bold text-center mb-8">
+            BostonHacks Admin Portal
+        </h2>
+        {/* <div style={{ ...cardStyles, display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
                 {Object.entries(statusCounts).map(([key, value]) => (
                     <div className=" shadow-lg rounded-lg p-5 flex flex-col items-center justify-center">
                         <span className="text-lg font-semibold">{key.charAt(0).toUpperCase() + key.slice(1)}</span>
                         <span className="text-2xl font-bold">{value}</span>
                     </div>
                 ))}
-            </div>
-            <div style={cardStyles}>
+            </div> */}
+        {/* <div style={cardStyles}>
                 <h1 className="text-3xl font-bold text-center mb-5">Applications</h1>
                 <DataGrid
                     rows={applicants.map(applicant => ({ ...applicant, id: applicant.id }))}
@@ -174,8 +177,37 @@ export default function ApplicationPage() {
                     disableSelectionOnClick
                     className="bg-white"
                 />
-            </div>
-            <ApplicantModal open={modalOpen} handleClose={handleCloseModal} applicant={selectedApplicant} />
-        </main>
-    );
+            </div> */}
+        <table className="min-w-full bg-gray-300 text-white border border-gray-700 divide-y divide-gray-700">
+            <thead>
+                <tr className="bg-gray-900 text-white border-b border-gray-700 divide-x divide-gray-700">
+                    <th className="px-4 py-2 text-2xl">First Name</th>
+                    <th className="px-4 py-2 text-2xl">Last Name</th>
+                    <th className="px-4 py-2 text-2xl">Email</th>
+                    <th className="px-4 py-2 text-2xl">School</th>
+                    <th className="px-4 py-2 text-2xl">GitHub</th>
+                    <th className="px-4 py-2 text-2xl">Application</th>
+                    <th className="px-4 py-2 text-2xl">Status</th>
+                </tr>
+            </thead>
+            <tbody className="divide-y divide-gray-700">
+                {rows.map((row) => (
+                    <tr key={row.id} className="divide-x divide-gray-700 text-gray-900">
+                        <td className="px-4 py-2">{row.firstName}</td>
+                        <td className="px-4 py-2">{row.lastName}</td>
+                        <td className="px-4 py-2">{row.email}</td>
+                        <td className="px-4 py-2">{row.schoolLabel}</td>
+                        <td className="px-4 py-2">{row.github}</td>
+                        <td className="px-4 py-2">
+                            <Button variant="contained" color="primary" onClick={() => handleOpenModal(row)}>
+                                View
+                            </Button>
+                        </td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+        <ApplicantModal open={modalOpen} handleClose={handleCloseModal} applicant={selectedApplicant} />
+    </main>
+);
 }
